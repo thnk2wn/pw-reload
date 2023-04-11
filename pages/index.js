2d09205eb364f7b0876eea5367a9115ac3572e02
Geoff Hudik <120529+thnk2wn@users.noreply.github.com>
2023-04-10 21:50:48 -0400

    button reload state change

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,41 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React, {useEffect, useState} from 'react';
 
 export default function Home() {
+  const getColor = () => window.localStorage.getItem('color' || 'white');
+  const [color, setColor] = useState();
+  const [disabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    setColor(getColor());
+  }, []);
+
+
+  const onClick = () => {
+    setDisabled(true);
+    const oldColor = getColor();
+    const newColor = oldColor === 'white' ? 'cyan' : 'white';
+    //setColor(newColor);
+    window.localStorage.setItem('color', newColor);
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  }
+
   return (
-    <div className="container">
+    <div className="container" style={{backgroundColor: color}}>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Playwright Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Playwright Test" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <button onClick={onClick} disabled={disabled}>Click Me</button>
         </p>
       </main>
 
